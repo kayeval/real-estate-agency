481feba7418da90b1e0288c8bc909c58423b14b2
rmit-s3671230-vivianna-ngo <42133080+rmit-s3671230-vivianna-ngo@users.noreply.github.com>
2019-09-07 12:35:46 +1000

    Test Case example

--- SEF-RealEstateAgency/src/abstracts/Property.java
+++ SEF-RealEstateAgency/src/abstracts/Property.java
@@ -1,5 +1,36 @@
 package abstracts;
 
+import java.util.Date;
+
 public abstract class Property {
+	
+	private String propertyID;
+	private double price;
+	private Date dateListed;
+	private boolean isActive;
+	//private HashMap<String, Proposal> proposals;
+	//acceptedProposal
+	private boolean documentsInspected;
+	private String address; 
+	private String suburb;
+	//private Capacity capacity;
+	//private Type type;
+	//private PropertyOwner propertyOwner;
+	
+	public Property(String propertyID, double price)
+	{
+		this.propertyID = propertyID;
+		this.price = price;
+	}
+	
+	public double getPrice()
+	{
+		return this.price;
+	}
+	
+	public String getPropertyID()
+	{
+		return this.propertyID;
+	}
 
 }
---
+++ SEF-RealEstateAgency/src/model/PropertyImpl.java
@@ -0,0 +1,13 @@
+package model;
+import abstracts.Property;
+
+public class PropertyImpl extends Property {
+
+	
+	public PropertyImpl(String propertyID, double price) {
+		super(propertyID, price);
+	}
+	
+
+
+}
